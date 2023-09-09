d7eae446cbdb22ba83b668451a336276a3d45cd0
ali-mansour <dev.ali.mansour@gmail.com>
2023-09-09 06:05:02 +0300

    refactor: Add Another Solution For Fin NOs Without using String Built-in Functions.

--- find_nos.cpp
+++ find_nos.cpp
@@ -8,16 +8,16 @@ int main() {
     while (N > 0) {
         string str;
         cin >> str;
-        int length = str.length();
-        if (length == 2) {
-            while (length > 0) {
-                if (((_tolower(str[0]) == _tolower('o') && (tolower(str[1]) == tolower('n'))) ||
-                     (_tolower(str[0]) == _tolower('n') && (tolower(str[1]) == tolower('o')))))
-                    cout << str << " ";
-                length--;
-            }
-        }
-        N--;
+        if (str == "on" || str == "ON" || str == "On" || str == "oN" ||
+            str == "no" || str == "NO" || str == "No" || str == "nO")
+            cout << str << " ";
+
+        /*if (str.length() == 2) {
+            if (((_tolower(str[0]) == _tolower('o') && (tolower(str[1]) == tolower('n'))) ||
+                 (_tolower(str[0]) == _tolower('n') && (tolower(str[1]) == tolower('o')))))
+                cout << str << " ";
+        }*/
+        --N;
     }
 
     return 0;
