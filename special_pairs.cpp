ba969ec8a222431e5042575aa603549012815893
ali-mansour <dev.ali.mansour@gmail.com>
2023-09-09 20:49:35 +0300

    refactor: Make Special Pair Solution In special_pairs.cpp Faster.

--- special_pairs.cpp
+++ special_pairs.cpp
@@ -5,8 +5,11 @@ using namespace std;
 int main() {
     int cnt = 0;
     for (int x = 50; x <= 300; ++x) {
-        for (int y = 70; y <= 400; ++y) {
-            if (x < y && (x + y) % 7 == 0)
+        int start = 70;
+        if (start < x + 1)
+            start = x + 1;
+        for (int y = start; y <= 400; ++y) {
+            if ((x + y) % 7 == 0)
                 ++cnt;
         }
     }
