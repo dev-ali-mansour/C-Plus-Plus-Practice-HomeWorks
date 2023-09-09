097392844520ec95d46cc21cf315818137918bd8
ali-mansour <dev.ali.mansour@gmail.com>
2023-09-09 20:45:14 +0300

    feat: Create special_pairs.cpp.

--- special_pairs.cpp
+++ special_pairs.cpp
@@ -7,9 +7,9 @@ int main() {
     for (int x = 50; x <= 300; ++x) {
         for (int y = 70; y <= 400; ++y) {
             if (x < y && (x + y) % 7 == 0)
-                cnt++;
+                ++cnt;
         }
     }
-    cout << cnt;
+    cout << cnt << "\n";
     return 0;
 }
