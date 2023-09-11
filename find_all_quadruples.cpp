29a24711271ad4308d1333540b7102dea817c3b6
ali-mansour <dev.ali.mansour@gmail.com>
2023-09-11 06:15:58 +0300

    refactor: Make find_all_quadruples.cpp Solution Faster Using Only 3 Loops.

--- find_all_quadruples.cpp
+++ find_all_quadruples.cpp
@@ -7,9 +7,9 @@ int main() {
     for (int a = 1; a <= 200; ++a) {
         for (int b = 1; b <= 200; ++b) {
             for (int c = 1; c <= 200; ++c) {
-                for (int d = 1; d <= 200; ++d) {
-                    count += (a + b == c + d);
-                }
+                int d = a + b - c;
+                if (1 <= d && d <= 200)
+                    count++;
             }
         }
     }
