15f92e650a2882204e2d2c15d95bb7a720ee90b0
ali-mansour <dev.ali.mansour@gmail.com>
2023-09-11 06:24:51 +0300

    feat: Create is_prime.cpp.

---
+++ is_prime.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number;
+    cin >> number;
+    if (number == 1) cout << "NO";
+    else {
+        bool is_prime = true;
+
+        for (int i = 2; i < number; ++i) {
+            if ((number % i) == 0) {
+                is_prime = false;
+                break;
+            }
+        }
+
+        if (is_prime)
+            cout << "YES";
+        else cout << "NO";
+    }
+    return 0;
+}
