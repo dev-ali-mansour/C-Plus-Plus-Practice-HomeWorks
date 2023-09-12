930d5a296f8f601c2274ce32bbd86ff5e2080b68
ali-mansour <dev.ali.mansour@gmail.com>
2023-09-12 06:33:28 +0300

    refactor: unique_numbers_ordered_list.cpp Using One Loop, And Without Using Array At All.

--- Array1D/unique_numbers_ordered_list.cpp
+++ Array1D/unique_numbers_ordered_list.cpp
@@ -3,17 +3,33 @@
 using namespace std;
 
 int main() {
-    int N;
+    /*int N;
     cin >> N;
     int numbers[N];
 
     for (int i = 0; i < N; ++i) {
+        int value;
         cin >> numbers[i];
+
     }
 
     for (int i = 0; i < N; ++i) {
         if (numbers[i] != numbers[i - 1])
             cout << numbers[i] << " ";
+    }*/
+
+    int N;
+    cin >> N;
+
+    int last_value = INT32_MIN;
+
+    for (int i = 0; i < N; ++i) {
+        int value;
+        cin >> value;
+        if (value != last_value) {
+            cout << value << " ";
+            last_value = value;
+        }
     }
 
     return 0;
