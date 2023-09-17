e58a31087c236a3d132d98512d222d726a2903eb
ali-mansour <dev.ali.mansour@gmail.com>
2023-09-17 19:12:38 +0300

    feat: Create is_suffix.cpp.

---
+++ CharArray/is_suffix.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string input, str;
+    cin >> input >> str;
+
+    if (str.size() > input.size()) {
+        cout << "NO\n";
+        return 0;
+    }
+
+    int start = input.size() - str.size();
+    for (int i = 0; i < str.size(); ++i) {
+        if (str[i] != input[i + start]) {
+            cout << "NO\n";
+            return 0;
+        }
+    }
+    cout << "YES\n";
+
+    return 0;
+}
