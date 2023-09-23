17881f2039b0ec3ee22695eb70fccd2ebf021d8f
ali-mansour <dev.ali.mansour@gmail.com>
2023-09-23 08:21:53 +0300

    refactor: Enhance Transpose Solution In transpose.cpp By Using Only One Array.

--- MultiDimensionalArray/transpose.cpp
+++ MultiDimensionalArray/transpose.cpp
@@ -3,20 +3,13 @@
 using namespace std;
 
 int main() {
-    int matrix[100][100];
     int trans[100][100];
     int n, m;
 
     cin >> n >> m;
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
-            cin >> matrix[i][j];
-        }
-    }
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            trans[j][i] = matrix[i][j];
+            cin >> trans[j][i];
         }
     }
 
@@ -26,7 +19,6 @@ int main() {
         }
         cout << endl;
     }
-
 
     return 0;
 }
