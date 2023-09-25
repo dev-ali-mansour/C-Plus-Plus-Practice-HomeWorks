dbc66a0775db7b1f345d5dabd235304e8fada03a
Ali Mansour <dev.ali.mansour@gmail.com>
2023-09-25 16:56:09 +0300

    feat: Create absolute.cpp.

---
+++ functions/absolute.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int absolute(int n) {
+    if (n >= 0)
+        return n;
+    return -n;
+}
+
+int main() {
+    cout << absolute(5) << endl;
+    cout << absolute(-5) << endl;
+
+    return 0;
+}
