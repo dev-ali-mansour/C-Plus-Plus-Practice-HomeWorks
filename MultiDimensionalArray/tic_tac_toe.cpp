cdc69959f88512061511653dcadc6e96d2da0b81
Ali Mansour <dev.ali.mansour@gmail.com>
2023-09-25 16:13:02 +0300

    refactor: Decrement User Input For Steps To Make It 1 Based, And Handle Secondary Diagonal Wins Situation.

--- MultiDimensionalArray/tic_tac_toe.cpp
+++ MultiDimensionalArray/tic_tac_toe.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 int main() {
-// Note This solution is too complex and not cover secondary diagonal win situation
-    int n;
+    int n, steps = 0;
     cin >> n;
 
     char arr[n][n];
@@ -64,6 +63,27 @@ int main() {
             return 0;
         }
 
+        int R = 0, C = n - 1;
+        is_same = true;
+        while (R < n - 1 && C > 0) {
+            if (arr[R][C] != arr[R + 1][C - 1] || (arr[R][C] == '.' && arr[R + 1][C - 1] == '.')) {
+                is_same = false;
+                break;
+            }
+            R++;
+            C--;
+        }
+        if (is_same) {
+            cout << current << " player won!\n";
+            return 0;
+        }
+
+
+        if (steps == n * n) {
+            cout << "tie\n";
+            return 0;
+        }
+
         if (current == 'x')
             current = 'o';
         else
@@ -73,10 +93,13 @@ int main() {
         int r, c;
 
         cin >> r >> c;
+        r--, c--;
         while (r > n || c > n || arr[r][c] != '.') {
             cout << "Invalid location! Try again: ";
             cin >> r >> c;
+            r--, c--;
         }
         arr[r][c] = current;
+        steps++;
     }
 }
