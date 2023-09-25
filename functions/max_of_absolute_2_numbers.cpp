f7ef7ef64c91bf5924b71b982438f08208e11a1c
Ali Mansour <dev.ali.mansour@gmail.com>
2023-09-25 17:06:21 +0300

    feat: Create max_of_absolute_2_numbers.cpp.

---
+++ functions/max_of_absolute_2_numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int absolute(int a) {
+    if (a >= 0)
+        return a;
+    return -a;
+}
+
+int max_abs(int a, int b) {
+    a = absolute(a);
+    b = absolute(b);
+
+    if (a > b)
+        return a;
+    return b;
+}
+
+int main() {
+    cout << max_abs(2, 5) << endl;
+    cout << max_abs(2, -5) << endl;
+
+    return 0;
+}
