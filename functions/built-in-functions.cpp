ff266ce98395dff42c66df6f93f415fe88bdcefb
ali-mansour <dev.ali.mansour@gmail.com>
2023-09-26 08:05:23 +0300

    refactor: Add More Built-in Functions To built-in-functions.cpp.

--- functions/built-in-functions.cpp
+++ functions/built-in-functions.cpp
@@ -18,5 +18,23 @@ int main() {
     cout << floor(-2.4) << endl;    // -3   **TRICKY
     cout << sqrt(16) << endl;       // 4
 
+    cout << isalpha('A') << endl;               // 1024 = true
+    cout << isalpha('#') << endl;               // 0
+    cout << isdigit('3') << endl;               // 1 = true
+    cout << isdigit('A') << endl;               // 0
+    cout << isupper('A') << endl;               // 256 = true
+    cout << isupper('a') << endl;               // 0
+    cout << (char) tolower('X') << endl;      // x
+    cout << (char) toupper('x') << endl;      // X
+
+    cout << max(5, 9) << endl;                       // 9
+    cout << pow(2, 4) << endl;                  // 16
+    cout << pow(2, 4.1) << endl;                // 17.1484
+    cout << log2(16) << endl;                    // 4
+    cout << log10(1000) << endl;                 // 3
+
+    cout << (double) rand() / RAND_MAX << endl;     //0.840188
+
+
     return 0;
 }
