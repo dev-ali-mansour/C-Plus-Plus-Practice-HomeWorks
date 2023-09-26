34e87b46b5d25804d235bc0ec7abd2b6f7ae06aa
ali-mansour <dev.ali.mansour@gmail.com>
2023-09-26 06:46:14 +0300

    feat: Create function-overloading.cpp.

---
+++ functions/function-overloading.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b) {
+    return a + b;
+}
+
+double add(double a, double b) {
+    return a + b;
+}
+
+int add(int a, int b, int c) {
+    return a + b + c;
+}
+
+int main() {
+    cout << add(2, 3) << endl;
+    cout << add(2.0, 3.5) << endl;
+    cout << add(2, 3, 5) << endl;
+
+    return 0;
+}
