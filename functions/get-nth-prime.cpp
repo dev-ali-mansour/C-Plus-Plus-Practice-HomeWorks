d74c2472bb5b88a947a167942a0f4e0109ae10ab
ali-mansour <dev.ali.mansour@gmail.com>
2023-09-28 15:54:01 +0300

    feat: Create get-nth-prime.cpp.

---
+++ functions/get-nth-prime.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+bool is_prime(int num) {
+    if (num <= 1)
+        return false;
+
+    for (int i = 2; i <= num / 2; ++i) {
+        if ((num % i) == 0)
+            return false;
+    }
+    return true;
+}
+
+int nth_prime(int n) {
+    for (int i = 2;; i++) {
+        if (is_prime(i)) {
+            --n;
+            if (n == 0)
+                return i;
+        }
+    }
+}
+
+int main() {
+
+    for (int i = 1; i < 20; ++i) {
+        cout << nth_prime(i) << " ";
+    }
+    return 0;
+}
