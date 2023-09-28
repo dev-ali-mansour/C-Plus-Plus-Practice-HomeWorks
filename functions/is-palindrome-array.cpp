eb66eb1bfa25dcd362431d354c2943ba2ffbc871
ali-mansour <dev.ali.mansour@gmail.com>
2023-09-28 15:30:27 +0300

    feat: create is-palindrome-array.cpp.

---
+++ functions/is-palindrome-array.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int read_array(int arr[]) {
+    int len;
+    cin >> len;
+
+    for (int i = 0; i < len; ++i)
+        cin >> arr[i];
+
+    return len;
+}
+
+bool is_palindrome(int n, const int arr[]) {
+    for (int i = 0; i < n / 2; ++i) {
+        if (arr[i] != arr[n - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int arr[100];
+    int len = read_array(arr);
+
+    if (is_palindrome(len, arr))
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+
+    return 0;
+}
