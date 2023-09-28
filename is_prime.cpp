d3444811487c892102f05b1302752b1bb882d463
ali-mansour <dev.ali.mansour@gmail.com>
2023-09-28 15:51:06 +0300

    fix: The Issue Of Checking If Number Is Prime In is_prime.cpp.

--- is_prime.cpp
+++ is_prime.cpp
@@ -9,7 +9,7 @@ int main() {
     else {
         bool is_prime = true;
 
-        for (int i = 2; i < number / 2; ++i) {
+        for (int i = 2; i <= number / 2; ++i) {
             if ((number % i) == 0) {
                 is_prime = false;
                 break;
