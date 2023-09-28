745b0de8cd6cd29ef7259c0e0197b0570e665f1a
ali-mansour <dev.ali.mansour@gmail.com>
2023-09-28 14:17:51 +0300

    create: max-of-6-numbers.cpp.

---
+++ functions/max-of-6-numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int max(int a, int b, int c) {
+    return max(a, max(b, c));
+}
+
+int max(int a, int b, int c, int d) {
+    return max(max(a, b, c), d);
+}
+
+int max(int a, int b, int c, int d, int e) {
+    return max(max(a, b, c, d), e);
+}
+
+int max(int a, int b, int c, int d, int e, int f) {
+    return max(max(a, b, c, d, e), f);
+}
+
+int main() {
+    cout << max(5, 10, 6, 22, 30, 50) << endl;
+    return 0;
+}
