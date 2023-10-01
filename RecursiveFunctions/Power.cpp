186c1bbea4871ffd2b26bf02a2ad5a6b63496866
ali-mansour <dev.ali.mansour@gmail.com>
2023-10-01 07:11:03 +0300

    feat: Create Power.cpp.

---
+++ RecursiveFunctions/Power.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int my_pow(int value, int power = 2) {
+    if (power == 0)
+        return 1;
+    return value * my_pow(value, power - 1);
+}
+
+int main() {
+    cout << my_pow(7, 3) << endl;
+    return 0;
+}
