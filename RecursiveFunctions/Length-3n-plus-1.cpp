aebae449d478c8366c184b5fa35007147db74301
ali-mansour <dev.ali.mansour@gmail.com>
2023-10-01 07:07:23 +0300

    feat: Create Length-3n-plus-1.cpp.

--- RecursiveFunctions/Length-3n-plus-1.cpp
+++ RecursiveFunctions/Length-3n-plus-1.cpp
@@ -9,8 +9,8 @@ int length_3n_plus_1(int n) {
 
     if (n % 2 == 0)
         return 1 + length_3n_plus_1(n / 2);
-    else
-        return 1 + length_3n_plus_1(3 * n + 1);
+
+    return 1 + length_3n_plus_1(3 * n + 1);
 }
 
 int main() {
