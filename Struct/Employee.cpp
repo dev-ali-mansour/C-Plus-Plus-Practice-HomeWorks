9941a11fa8a5cc41932d4b8b98d607552c723164
ali-mansour <dev.ali.mansour@gmail.com>
2023-10-04 15:40:03 +0300

    feat: Create Employee.cpp.

---
+++ Struct/Employee.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+struct employee {
+    string name;
+    int age;
+    double salary;
+    char gender;
+
+    void read_employee() {
+        cout << "Enter employee 4 entries: ";
+        cin >> name >> age >> salary >> gender;
+    }
+
+    void print_employee() {
+        cout << name << " has salary " << salary << endl;
+    }
+
+    int get_age() {
+        return age;
+    }
+
+    void set_age(int new_age) {
+        age = new_age;
+    }
+};
+
+const int MAX = 10000;
+employee employees_arr[MAX];
+
+int added = 0; // Number of employees
+
+void print_employees() {
+    for (int i = 0; i < added; ++i) {
+        employees_arr[i].print_employee();
+    }
+}
+
+int main() {
+    employees_arr[added++].read_employee();
+    employees_arr[added++].read_employee();
+    print_employees();
+
+    return 0;
+}
