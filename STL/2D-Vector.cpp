2af0bc5f07ec7a282299a34061a6b578ea35feab
ali-mansour <dev.ali.mansour@gmail.com>
2023-10-10 08:04:39 +0300

    feat: Create 2D-Vector.cpp.

---
+++ STL/2D-Vector.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void test1() {
+    vector<string> matrix{"Ali", "Ahmed", "Sara"};
+
+    // String is 1D array of chars
+    // What about vector of string?
+    // Matrix of 3 rows, each one has dynamic columns
+    // Ali
+    // Ahmed
+    // Sara
+
+    cout << matrix.size() << "\n";      // 3 rows
+    int rows = matrix.size();
+    for (int i = 0; i < rows; ++i) {
+        // Dynamic column sizes: 3 5 4
+        cout << matrix[i].size() << " ";
+    }
+    cout << endl;
+}
+
+void print(vector<vector<int>> &v2d) {
+    for (auto &row: v2d) { // auto = vector<int>
+        for (auto &col: row) {    // auto = int
+            cout << col << " ";
+        }
+        cout << endl;
+    }
+    cout << endl << endl;
+}
+
+void test2() {
+    // 2D Array using vector
+    // (5,7) 2D array
+    vector<int> row(5, 1); // 5 numbers, each is 1
+
+    // 3 rows: each is 5 values of 1
+    vector<vector<int>> v2d(3, row);
+
+    print(v2d);
+
+    int rows = v2d.size(), cols = v2d[0].size();    // 3, 5
+
+    v2d[0][0] = 9;
+    v2d[1].push_back(5);
+    v2d[1].push_back(6);
+    v2d[2].erase(v2d[2].begin());
+    print(v2d);
+}
+
+int main() {
+    test1();
+    test2();
+
+    return 0;
+}
