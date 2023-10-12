b7669aea063354db6a807251079607dd3a32ee8a
ali-mansour <dev.ali.mansour@gmail.com>
2023-10-12 10:24:49 +0300

    refactor: Using STL In Hospital System To Enhance Its Code & Make It More Efficient.

--- Projects/Hospital-System-Using-Struct.cpp
+++ Projects/Hospital-System-Using-Struct.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <deque>
+#include <vector>
 
 using namespace std;
 const int MAX_SPECIALIZATION = 20;
@@ -9,74 +11,62 @@ const int GET = 3;
 const int EXIT = 4;
 
 struct hospital_queue {
-    string names[MAX_QUEUE];
-    int status[MAX_QUEUE];
-    int len;
+    // We might use priority_queue<pair<int, string>> que;
+    // for direct insert front/back
+    // But it is slower + won't allow printing
+
+    deque<pair<string, int>> que;
     int spec;
 
     hospital_queue() {
-        len = 0;
         spec = -1;
     }
 
     hospital_queue(int _spec) {
-        len = 0;
         spec = _spec;
     }
 
-    bool add_end(string name, int st) {
-        if (len == MAX_QUEUE)
-            return false;
-        names[len] = name, status[len] = st, ++len;
+    bool add_end(string &name, int st) {
+        auto item = make_pair(name, st);
+        que.push_back(item);
         return true;
     }
 
-    bool add_front(string name, int st) {
-        if (len == MAX_QUEUE)
-            return false;
-
-        // Shift right
-        for (int i = len; i > 0; --i) {
-            names[i] = names[i - 1];
-            status[i] = status[i - 1];
-        }
-        names[0] = name, status[0] = st, ++len;
+    bool add_front(string &name, int st) {
+        auto item = make_pair(name, st);
+        que.push_front(item);
         return true;
     }
 
     void remove_front() {
-        if (len == 0) {
+        if (que.empty()) {
             cout << "No patients at the moment. Have rest, Dr\n";
             return;
         }
-        cout << names[0] << " Please go with the Dr\n";
-
-        // Shift left
-        for (int i = 0; i < len; ++i) {
-            names[i] = names[i + 1];
-            status[i] = status[i + 1];
-        }
-        --len;
+        auto item = que.front();
+        que.pop_front();
+        cout << item.first << " Please go with the Dr\n";
     }
 
     void print() {
-        if (len == 0) return;
-        cout << "There are " << len << " patients in specialization " << spec << endl;
-        for (int i = 0; i < len; ++i) {
-            cout << names[i] << " ";
-            if (status[i])
+        if (que.empty()) return;
+        cout << "There are " << que.size() << " patients in specialization " << spec << endl;
+        for (auto &item: que) {
+            cout << item.first << " ";
+            if (item.second)
                 cout << "urgent\n";
             else cout << "regular\n";
         }
         cout << endl;
     }
-
 };
 
 struct hospital_system {
-    hospital_queue queues[MAX_SPECIALIZATION];
+    vector<hospital_queue> queues;
 
     hospital_system() {
+        queues = vector<hospital_queue>(MAX_SPECIALIZATION);
+
         for (int i = 0; i < MAX_SPECIALIZATION; ++i) {
             queues[i] = hospital_queue(i);
         }
@@ -119,17 +109,14 @@ struct hospital_system {
         cout << "Enter specialization, names, and status: ";
         cin >> spec >> name >> st;
 
-        bool status;
-        if (st == 0)
-            status = queues[spec].add_end(name, st);
-        else
-            status = queues[spec].add_front(name, st);
-
-        if (!status) {
+        if (queues[spec].que.size() == MAX_QUEUE) {
             cout << "Sorry We can't add more patients for this specialization.\n";
             return false;
         }
-
+        if (st == 0)
+            queues[spec].add_end(name, st);
+        else
+            queues[spec].add_front(name, st);
         return true;
     }
 
