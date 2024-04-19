cd010ed48d04dd895424dc5069a0f06e8d669a6c
Ali Mansour <dev.ali.mansour@gmail.com>
2024-04-19 22:45:14 +0200

    refactor: Use Function Prototype In absolute.cpp.

--- functions/absolute.cpp
+++ functions/absolute.cpp
@@ -2,15 +2,18 @@
 
 using namespace std;
 
-int absolute(int n) {
-    if (n >= 0)
-        return n;
-    return -n;
-}
+int absolute(int n);
 
 int main() {
     cout << absolute(5) << endl;
     cout << absolute(-5) << endl;
 
     return 0;
+}
+
+
+int absolute(int n) {
+    if (n >= 0)
+        return n;
+    return -n;
 }
