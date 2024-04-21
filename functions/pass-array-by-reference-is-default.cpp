7627abe535839ac7fdfaf685dd9128c31fbda44a
Ali Mansour <dev.ali.mansour@gmail.com>
2024-04-21 03:40:49 +0200

    refactor: Use Function Prototype In pass-array-by-reference-is-default.cpp.

--- functions/pass-array-by-reference-is-default.cpp
+++ functions/pass-array-by-reference-is-default.cpp
@@ -2,13 +2,7 @@
 
 using namespace std;
 
-int sum_array(int arr[], int len) {
-    int sum = 0;
-    for (int i = 0; i < len; ++i) {
-        sum += arr[i];
-    }
-    return sum;
-}
+int sum_array(int arr[], int len);
 
 int main() {
     int arr[6] = {1, 2, 3, 4, 5, 6};
@@ -16,4 +10,12 @@ int main() {
     cout << sum_array(arr, 6) << endl;
 
     return 0;
+}
+
+int sum_array(int arr[], int len) {
+    int sum = 0;
+    for (int i = 0; i < len; ++i) {
+        sum += arr[i];
+    }
+    return sum;
 }
