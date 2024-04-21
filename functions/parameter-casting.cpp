2e8266ede65c8d97b4fe614239dc7050f61b7c6c
Ali Mansour <dev.ali.mansour@gmail.com>
2024-04-21 03:39:56 +0200

    refactor: Use Function Prototype parameter-casting.cpp.

--- functions/parameter-casting.cpp
+++ functions/parameter-casting.cpp
@@ -2,13 +2,15 @@
 
 using namespace std;
 
-int sum(int a, int b) {
-    return a + b;
-}
+int sum(int a, int b);
 
 int main() {
     cout << sum(2, 3) << endl;
     cout << sum(2, 3.5) << endl;
 
     return 0;
+}
+
+int sum(int a, int b) {
+    return a + b;
 }
