46e8e41fb1055732e6f8a0f826346cef5ef88b65
Ali Mansour <dev.ali.mansour@gmail.com>
2024-04-21 03:39:15 +0200

    refactor: Use Function Prototype maximum-of-2-numbers.cpp.

--- functions/maximum-of-2-numbers.cpp
+++ functions/maximum-of-2-numbers.cpp
@@ -2,15 +2,17 @@
 
 using namespace std;
 
-int our_max(int a, int b) {
-    if (a >= b)
-        return a;
-    return b;
-}
+int our_max(int a, int b);
 
 int main() {
     cout << our_max(2, 5) << endl;
     cout << our_max(2, -5) << endl;
 
     return 0;
+}
+
+int our_max(int a, int b) {
+    if (a >= b)
+        return a;
+    return b;
 }
