f1028bd29b6c66e54ea359647c36e68a44c20313
Ali Mansour <dev.ali.mansour@gmail.com>
2024-07-03 07:53:04 +0300

    refactor: Separate Interface From Implementation By Moving Implementation Of sum_1_n() In sum.cpp File.

---
+++ HeadersAndIncludes/sum.cpp
@@ -0,0 +1,8 @@
+int sum_1_n(int n) {
+    int sum = 0;
+
+    for (int i = 0; i < n; ++i)
+        sum += i;
+
+    return sum;
+}
--- HeadersAndIncludes/sum.h
+++ HeadersAndIncludes/sum.h
@@ -2,13 +2,6 @@
 #ifndef HOMEWORKS_SUM_H
 #define HOMEWORKS_SUM_H
 
-int sum_1_n(int n) {
-    int sum = 0;
-
-    for (int i = 0; i < n; ++i)
-        sum += i;
-
-    return sum;
-}
+int sum_1_n(int n);
 
 #endif //HOMEWORKS_SUM_H
