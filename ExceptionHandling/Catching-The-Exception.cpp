f103ff4a7468ac0e63331a99431e7eff7cc8d3a4
Ali Mansour <dev.ali.mansour@gmail.com>
2024-07-03 13:45:51 +0300

    refactor: Create Catching-The-Exception.cpp.

---
+++ ExceptionHandling/Catching-The-Exception.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+struct BankAccount {
+    int money = 1000;
+
+    bool PayBill(int bill_value) {
+        if (bill_value < 0)
+            throw invalid_argument("Bill value can't be negative!");
+        if (money >= bill_value) {
+            money -= bill_value;
+            return true;
+        } else return false;
+    }
+};
+
+int main() {
+    BankAccount acct;
+    try {
+        cout << acct.PayBill(100) << endl;
+        cout << acct.PayBill(500000) << endl;
+        cout << acct.PayBill(-10) << endl;
+        cout << acct.PayBill(200) << endl;  // Won't be processed
+    }
+    catch (invalid_argument &e) {
+        cout << "Caught an exception: " << e.what() << endl;
+        throw e;    // I don't want to handle
+    }
+    cout << "Bye";
+
+    return 0;
+}
