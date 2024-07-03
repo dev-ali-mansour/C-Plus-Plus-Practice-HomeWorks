1078f6c19b91885831a0131f6507a30e03d78584
Ali Mansour <dev.ali.mansour@gmail.com>
2024-07-03 07:10:33 +0300

    feat: Create include.cpp.

---
+++ HeadersAndIncludes/include.cpp
@@ -0,0 +1,23 @@
+#include <iostream>     // cout
+#include <cmath>        // sqrt
+#include <vector>       // vector
+
+using namespace std;
+
+int sum_1_2(int n) {
+    int sum = 0;
+
+    for (int i = 0; i < n; ++i)
+        sum += i;
+
+    return sum;
+}
+
+int main() {
+    cout << sqrt(25) << endl;
+    vector<int> v;
+
+    cout << sum_1_2(5) << endl;
+
+    return 0;
+}
