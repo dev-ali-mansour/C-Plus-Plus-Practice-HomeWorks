02dc94c368832be467e5be6cbfdad08c22260584
Ali Mansour <dev.ali.mansour@gmail.com>
2024-07-03 14:45:47 +0300

    refactor: Create Throwing-Integer.cpp.

---
+++ ExceptionHandling/Throwing-Integer.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+// Create a dynamic array
+// Throws 3 possible exceptions
+int *CreateArrayOnMobile(int len) {
+    if (len <= 0)
+        throw 17;   // Some error code
+
+    if (len >= 10000)
+        throw runtime_error("Too big array for this mobile memory!");
+    return new int[len];
+}
+
+int main() {
+    int *p = nullptr;
+    try {
+        int *p = CreateArrayOnMobile(-10);
+    } catch (invalid_argument &e) {
+        cout << "invalid_argument: " << e.what();
+    } catch (runtime_error &e) {
+        cout << "runtime_error" << e.what();
+    } catch (...) {
+        cout << "Caught unknown exception type!\n";
+    }
+    if (p != nullptr) { // Only if created
+        delete[]p;
+        p = nullptr;
+    }
+
+    return 0;
+}
