f0cd14d8ae2332481eec6905bac4f301686d50d1
Ali Mansour <dev.ali.mansour@gmail.com>
2024-07-10 11:20:52 +0300

    refactor: Create Inheriting-Constructors.cpp.

---
+++ Inheritance/Inheriting-Constructors.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+public:
+    int x;
+    int y;
+
+    A(int x, int y = 20) : x(x), y(y) {}
+};
+
+class B : public A {
+    int z;
+public:
+    // C++1 inheriting base class constructors
+    using A::A;
+};
+
+int main(){
+    B b(3);
+    cout<<b.x<<" "<<b.y<<endl;  // 3 20
+    return 0;
+}
