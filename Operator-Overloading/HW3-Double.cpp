44b3c15e53c26137d3f07b8c78ea2891f635b6b8
Ali Mansour <dev.ali.mansour@gmail.com>
2024-07-18 18:59:58 +0300

    refactor: Create HW3-Double.cpp.

---
+++ Operator-Overloading/HW3-Double.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+class Double {
+public:
+    Double(double val) : value(val) {}
+
+    bool operator==(const Double& other) const {
+        return std::abs(value - other.value) < 1e-9;  // Tolerance for floating-point comparison
+    }
+
+    bool operator<(const Double& other) const {
+        return value < other.value;
+    }
+
+    double value;
+};
+
+
+int main() {
+    double d1 = 1 + 3.0 / 7.0 - 1;
+    double d2 = 3.0 / 7.0;
+
+    // 0.428571  0.428571    0
+    // If gave true, play with similar examples
+    cout << d1 << " " << d2 << " " << (d1 == d2) << endl;
+
+    Double cd1(d1);
+    Double cd2(d2);
+
+    cout << (cd1 == cd2) << endl;    // true
+
+    map<Double, string> map;
+    map[cd1]=10;
+    map[cd2] = 20;
+
+    cout << map.size();   // 2
+
+}
