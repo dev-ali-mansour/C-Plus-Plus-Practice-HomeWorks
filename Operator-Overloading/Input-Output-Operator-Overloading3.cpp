d74a6e707237cdd440ba3d1007ce03884eae9e55
Ali Mansour <dev.ali.mansour@gmail.com>
2024-07-18 22:14:07 +0300

    refactor: Create Input-Output-Operator-Overloading3.cpp.

---
+++ Operator-Overloading/Input-Output-Operator-Overloading3.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+
+using namespace std;
+
+class MyPair {
+private:
+    int first, second;
+public:
+    MyPair() :    // Empty constructor to allow using default
+            first(-1), second(-1) {
+    }
+
+    MyPair(int first_, int second_) : first(first_), second(second_) {}
+
+    void print() {
+        cout << "(" << first << "," << second << ")\n";
+    }
+
+    int operator[](int pos) const {
+        if (pos == 0) return first;
+        return second;
+    }
+
+    int &operator[](int pos) {
+        if (pos == 0) return first;
+        return second;
+    }
+
+    // ++x prefix increment operator
+    MyPair &operator++() {
+        ++first, ++second;   // Increment first
+        return *this;       // Then return reference
+    }
+
+    // x++ postfix increment operator
+    // DON'T return MyPair&: this is local variable
+    MyPair operator++(int) {
+        MyPair cpy = *this;   //Copy first
+        ++first, ++second;
+        return cpy;
+    }
+
+    int getFirst() const {
+        return first;
+    }
+
+    void setFirst(int first) {
+        MyPair::first = first;
+    }
+
+    int getSecond() const {
+        return second;
+    }
+
+    void setSecond(int second) {
+        MyPair::second = second;
+    }
+
+    friend istream &operator>>(istream &input, MyPair &pair);
+
+    friend ostream &operator<<(ostream &ouput, const MyPair &pair);
+
+};
+
+istream &operator>>(istream &input, MyPair &pair) {
+    input >> pair.first >> pair.second;
+    return input;
+}
+
+ostream &operator<<(ostream &output, const MyPair &pair) {
+    output << pair.first <<" "<< pair.second<<endl;
+    return output;
+}
+
+int main() {
+    MyPair x, y;
+    cin >> x >> y;
+
+    cout << x << y;
+
+    return 0;
+}
